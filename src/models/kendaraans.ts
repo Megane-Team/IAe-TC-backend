fbaab678bffae2ca85eb0dec928239aa4d69638c
Maulana Yusuf <myalghani@gmail.com>
2024-11-01 09:24:06 +0700

    feat: adding kendaraan models

---
+++ src/models/kendaraans.ts
@@ -0,0 +1,21 @@
+import { sql } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+export const kendaraans = pgTable("kendaraans", {
+    id: integer().generatedAlwaysAsIdentity().primaryKey(),
+    name: text().notNull(),
+    plat: text().notNull().unique(),
+    status: text().notNull(),
+    condition: text().notNull(),
+    warranty: text().notNull(),
+    category: text().notNull(),
+    coloer: text().notNull(),
+    photo: text(),
+    created_at: timestamp({ withTimezone: true }).notNull().default(sql`now()`)
+})
+
+export const kendaraanSchema = {
+    insert: createInsertSchema(kendaraans),
+    select: createSelectSchema(kendaraans)
+}
