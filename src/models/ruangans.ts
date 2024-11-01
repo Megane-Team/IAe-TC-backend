276d3aeddf49dddbd091ef410dba4bc338a98dd1
Maulana Yusuf <myalghani@gmail.com>
2024-11-01 09:18:34 +0700

    adding ruangan models

---
+++ src/models/ruangans.ts
@@ -0,0 +1,20 @@
+import { sql } from "drizzle-orm";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { barangs } from "./barangs.js";
+
+export const ruangans = pgTable("ruangans", {
+    id: integer().generatedAlwaysAsIdentity().primaryKey(),
+    code: text().notNull(),
+    status: text().notNull(),
+    capacity: integer().notNull(),
+    category: text().notNull(),
+    photo: text().notNull(),
+    created_at: timestamp({ withTimezone: true }).notNull().default(sql`now()`),
+    id_barang: integer().notNull().references(() => barangs.id)
+})
+
+export const ruanganSchema = {
+    insert: createInsertSchema(ruangans),
+    select: createSelectSchema(ruangans)
+}
