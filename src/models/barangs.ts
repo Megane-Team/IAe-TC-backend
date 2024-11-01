1eba692a4e3628a4c7d58b4903ecb4bc22b74a15
Maulana Yusuf <myalghani@gmail.com>
2024-11-01 09:15:28 +0700

    feat: adding barangs models

---
+++ src/models/barangs.ts
@@ -0,0 +1,19 @@
+import { sql } from "drizzle-orm";
+import { date, integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+
+export const barangs = pgTable("barangs", {
+    id: integer().generatedAlwaysAsIdentity().primaryKey(),
+    name: text().notNull(),
+    code: text().notNull(),
+    status: text().notNull(),
+    condition: text().notNull(),
+    warranty: date().notNull(),
+    photo: text().notNull(),
+    created_at: timestamp({ withTimezone: true }).notNull().default(sql`now()`)
+})
+
+export const barangSchema = {
+    insert: createInsertSchema(barangs),
+    select: createSelectSchema(barangs)
+}
