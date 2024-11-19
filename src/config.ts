084e8ae84d92442602f1a8ebc674573163b2c833
Maulana Yusuf <myalghani@gmail.com>
2024-11-19 18:21:03 +0700

    feat: the remaining data

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,6 @@ export const databaseUrl = process.env.DATABASE_URL;
 export const databaseMaxPool = Number(process.env.DATABASE_MAX_POOL || 50);
 
 export const secretToken = process.env.JWT_TOKEN
-
 export const defaultPassword = process.env.DEFAULT_PASSWORD;
 
 if (!databaseUrl) {
