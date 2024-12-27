f1ef6150bcc76f04066781d813e34a85c5d5eb8a
Maulana Yusuf <myalghani@gmail.com>
2024-12-27 13:13:29 +0700

    refactor: JWT_TOKEN - JWT_SECRET

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const host = process.env.HOST || "127.0.0.1";
 export const databaseUrl = process.env.DATABASE_URL;
 export const databaseMaxPool = Number(process.env.DATABASE_MAX_POOL || 50);
 
-export const secretToken = process.env.JWT_TOKEN
+export const secretToken = process.env.JWT_SECRET;
 export const defaultPassword = process.env.DEFAULT_PASSWORD;
 
 if (!databaseUrl) {
